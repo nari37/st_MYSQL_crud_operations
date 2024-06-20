46573bdc34422c1dddb84b9e0f101b27dbfde6d7
nari37 <nareshpogiri409@gmail.com>
2024-06-20 16:08:47 +0530

    Your commit message

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,19 @@
 import express from 'express'
 import mysql from 'mysql'
 import cors from 'cors'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json())
 const db = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'students'
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 db.connect((err) => {
     if (err) {
