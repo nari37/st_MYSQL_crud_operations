688bd4cdb9496331b64c37a29b53e86bc8072960
nari37 <116661106+nari37@users.noreply.github.com>
2024-09-02 10:16:46 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors(({ origin: 'https://mongo-crud-eiaj.onrender.com' })));
 app.use(express.json());
 
 const port = process.env.Port || 8000;
